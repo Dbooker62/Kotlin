d210ef7016ac0addaef3c0be81bce80abd896843
Dbooker62 <123719363+Dbooker62@users.noreply.github.com>
2023-08-21 01:00:25 -0500

    Create Datatype.kt

---
+++ Datatype.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    val myInt: Int = 42
+    println("Integer: $myInt")
+
+
+    val myDouble: Double = 3.14
+    println("Double: $myDouble")
+
+
+    var myChar: Char = 'A'
+    println("Character: $myChar")
+
+
+    var myBoolean: Boolean = true
+    println("Boolean: $myBoolean")
+
+
+    var myString: String = "Hello, Kotlin!"
+    println("String: $myString")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+
+}
