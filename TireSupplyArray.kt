0c777559d5e4142b88cb6f76d4c1247f04de4a49
Dbooker62 <123719363+Dbooker62@users.noreply.github.com>
2023-08-30 09:28:52 -0500

    Create TireSupplyArray.kt

---
+++ TireSupplyArray.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val supplies = arrayOf("Tires", "Rims", "Valve Stems", "Tire Gauges", "Wheel Balancer", "Tire Changer", "Jack Stands", "Lug Wrenches", "Tire Repair Kits", "Air Compressor")
+
+    println("Supplies for the Tire Shop:")
+    for (item in supplies) {
+        println(item)
+    }
+}
